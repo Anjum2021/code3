98b96799f83fc408b14d4ec5c0032059634d8962
Anjum2021 <anjumrnadaf086@gmail.com>
2025-05-06 14:52:29 +0530

    save my file to master branch

--- Employee.java
+++ Employee.java
@@ -2,19 +2,22 @@ public class Employee{
     String name;
     int id;
     double salary;
-    public Employee(String name,int id,double salary){
+    int age;
+    public Employee(String name,int id,double salary,int age){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.age=age;
     }
     public void displayInfo(){
         System.out.println("Employee name:"+name);
         System.out.println("Employee id:"+id);
         System.out.println("Employee salary:"+salary);
+        System.out.println("Employee age:"+age);
 
     }
     public static void main(String[] args){
-        Employee emp1=new Employee("xyz",101,500000.0 );
+        Employee emp1=new Employee("xyz",101,500000.0 ,24);
         emp1.displayInfo();
     }
 }
